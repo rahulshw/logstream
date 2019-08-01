758ed01ecf0230eb048fce251e9b93b6e5b52353
Rahul Shaw <rahul.shaw@inmobi.com>
2019-08-01 22:35:05 +0530

    coroutine based file reading

--- app/app.py
+++ app/app.py
@@ -5,7 +5,6 @@ from tornado.ioloop import IOLoop
 import tornado.web
 import tornado.gen
 import tornado.log
-import socket
 import os
 import uuid
 from helpers import seek2
@@ -16,7 +15,7 @@ PINGINTERVAL = int(os.environ.get('PINGINTERVAL', 5))  # websocket ping interval
 PINGTIMEOUT = int(os.environ.get('PINGTIMEOUT', 10))  # websocket timeout duration in second
 PORT = int(os.environ.get('PORT', 8888))  # webserver port
 
-
+@tornado.gen.coroutine
 def read_and_send_file(request, filename, nlines):
     """
     emulates tail -f sending the content through websockets.
@@ -40,10 +39,8 @@ def read_and_send_file(request, filename, nlines):
                 request.logger.error('Error occurred while sending data for connection:%s, reason: %s' % (request.id, e))
                 request.close(code=400, reason=str(e))
                 f.close()
-            except ValueError as e:
-                request.logger.error('Error occurred while reading file for connection:%s, reason: %s' %(request.id, e))
-                raise e
-            tornado.gen.sleep(LOGBUFFER)
+                raise Exception('discarding thread')
+            yield tornado.gen.sleep(LOGBUFFER)
 
 
 class WSHandler(tornado.websocket.WebSocketHandler):
@@ -53,7 +50,7 @@ class WSHandler(tornado.websocket.WebSocketHandler):
         super().__init__(*args, **kwargs)
         self.logger = tornado.log.app_log
         self.logger.setLevel('INFO')
-        self.id = uuid.uuid4()
+        self.id = uuid.uuid4()  # handler object identifier
         self.nlines = 0
         self.filename = ''
 
@@ -61,12 +58,12 @@ class WSHandler(tornado.websocket.WebSocketHandler):
         self.nlines = int(self.get_query_argument('l'))
         self.filename = self.get_query_argument('f')
         self.logger.info('new connection: %s, from: %s' % (self.id, self.request.remote_ip))
-        #read_and_send_file(self, self.filename, self.nlines)
 
     def on_message(self, message):
-        print('message: %s'%message)
         if message == 'start_sending':
             read_and_send_file(self, self.filename, self.nlines)
+        else:
+            self.logger.info('received invalid message: %s' % message)
 
     def on_close(self):
         self.logger.info('connection:%s closed' % self.id)
@@ -99,7 +96,7 @@ application = tornado.web.Application(
 if __name__ == "__main__":
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(PORT)
-    http_server.start(num_processes=1, max_restarts=1)
+    http_server.start(num_processes=1, max_restarts=3)
     print('server started')
     IOLoop.current().start()
 
